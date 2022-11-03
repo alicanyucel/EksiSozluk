289a7278e55f14dddd7ea94f6d9b370b2c4f5b66
ali can yücel <yucelalican@hotmail.com>
2022-11-03 15:29:09 +0300

    Temanın Gerekli Yerleri Türkçeleştirildi.

---
+++ MvcProjeKampi/MvcProjeKampi/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public PartialViewResult PartialView1()
+        {
+            return PartialView();
+        }
+    }
+}
