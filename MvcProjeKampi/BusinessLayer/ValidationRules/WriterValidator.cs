c8846e8fe97e7b7ecb1a35bc210264ded91c2103
ali can yücel <yucelalican@hotmail.com>
2022-11-07 16:48:19 +0300

    35/150 Completed

---
+++ MvcProjeKampi/BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentValidation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("yazar ad ve soyadı boş geçilemez");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("şifre boş geçilemez");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("lütfen yazar ismini en az 2 karakter giriniz");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("yazar ismi 50 karakteri geçmemeli");
+        }
+    }
+}
---
+++ MvcProjeKampi/MvcProjeKampi/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFrameWork;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        // ekleme işlemlerinde httpget ve httpost attributeleri olan
+        // metotların ismi aynı olmalıdır.
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Writer p)
+        {
+            WriterValidator vm = new WriterValidator();
+            ValidationResult result=vm.Validate(p);
+            if (result.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "deneme test";
+                wm.WriterAdd(p);
+
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach(var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+                return View();
+        }
+    }
+}
