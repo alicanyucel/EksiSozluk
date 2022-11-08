f6e0f011f4b029c63b30226fe43bb7732c121bcc
ali can yücel <yucelalican@hotmail.com>
2022-11-08 20:13:32 +0300

    ...

---
+++ MvcProjeKampi/MvcProjeKampi/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            var values = abm.GetList();
+            return PartialView();
+        }
+    }
+}
