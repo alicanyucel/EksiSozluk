74a4260f72d4dfa45665ef1d3a7f6f3bb51498f5
ali can yücel <yucelalican@hotmail.com>
2022-11-09 19:54:14 +0300

    45/150 Completed.

--- MvcProjeKampi/MvcProjeKampi/Startup.cs
+++ MvcProjeKampi/MvcProjeKampi/Startup.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,12 @@ namespace MvcProjeKampi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc(config=>
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                config.Filters.Add(new AuthorizeFilter(policy)); 
+            });
             services.AddControllersWithViews();
         }
 
