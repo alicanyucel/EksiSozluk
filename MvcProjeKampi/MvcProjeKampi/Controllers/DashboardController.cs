bb786335e433158f1af9c162dddabc977c68349a
ali can yücel <yucelalican@hotmail.com>
2022-11-15 15:57:38 +0300

    62/150 Completed.

--- MvcProjeKampi/MvcProjeKampi/Controllers/DashboardController.cs
+++ MvcProjeKampi/MvcProjeKampi/Controllers/DashboardController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace MvcProjeKampi.Controllers
 {
+    [AllowAnonymous]
     public class DashboardController : Controller
     {
         public IActionResult Index()
